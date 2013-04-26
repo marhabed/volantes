d1717bc139597afb1044931fe88b64e210d5c1c9
marhabed <marhabed2@gmail.com>
2013-04-26 15:19:57 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Volantes::Application.config.secret_token = '9fac555fef3ee5a68f911cd44d6b0a62bf255514cd995c27cefe9c0d52f902edc3b6280ebd1fe19c69bd72f8a34c697f82793ae90cd01aac12841d4def02be88'
